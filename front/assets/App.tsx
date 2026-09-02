e5ff8d764f7cce9cc20742d7d3f7e2fed64e86bb
TechnolyGames <technolygames@gmail.com>
2026-09-02 14:04:55 -0600

    Login agregado y cambios varios

--- front/assets/App.tsx
+++ front/assets/App.tsx
@@ -1,12 +1,11 @@
 import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
+import heroImg from './image/hero.png'
+import reactLogo from './images/react.svg'
+import viteLogo from './images/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
   return (
     <>
       <section id="center">
